794678ff14d39cec8b6ee2bccfae217bd31577d3
hugo espinelli <hugo.espinelli@g.globo>
2024-04-16 23:36:40 -0300

    added pagination to every route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,26 +1,27 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.page(params[:page]).per(params[:per_page] || 10)
     render json: @movies
   end
 
   def recommendations
-    favorite_movies = User.find(params[:user_id]).favorites
+    favorite_movies = User.find(params[:user_id]).favorites.page(params[:page]).per(params[:per_page] || 10)
     @recommendations = RecommendationEngine.new(favorite_movies).recommendations
     render json: @recommendations
   end
 
   def user_rented_movies
-    @rented = User.find(params[:user_id]).rented
+    @rented = User.find(params[:user_id]).rented.page(params[:page]).per(params[:per_page] || 10)
     render json: @rented
   end
 
   def rent
-    user = User.find(params[:user_id])
     movie = Movie.find(params[:id])
     movie.available_copies -= 1
+    user = User.find(params[:user_id])
     movie.save
     user.rented << movie
     render json: movie
+    
   end
 end
